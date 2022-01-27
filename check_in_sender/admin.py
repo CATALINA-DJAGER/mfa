0fc3780365a92bb00258cf82a8a04a120980bfb8
CATALINA-DJAGER <atychka2@gmail.com>
2022-01-27 09:38:14 +0200

    optimize models (delete Template, add EmailTraveller) add fixtures and admin

--- check_in_sender/admin.py
+++ check_in_sender/admin.py
@@ -1,10 +1,31 @@
 from django.contrib import admin
 
-from .models import Aircompany
+from .models import Aircompany, Traveller, CheckIn, Email, EmailTraveller
 
 
 class AircompanyAdmin(admin.ModelAdmin):
     list_display = ('name', 'code')
 
 
+class TravellerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email')
+
+
+class CheckInAdmin(admin.ModelAdmin):
+    list_display = ('aircompany', 'traveller', 'date_time', 'status')
+
+
+class EmailAdmin(admin.ModelAdmin):
+    list_display = ('template_path', 'content')
+
+
+class EmailTravellerAdmin(admin.ModelAdmin):
+    list_display = ('email', 'traveller')
+
+
 admin.site.register(Aircompany, AircompanyAdmin)
+admin.site.register(Traveller, TravellerAdmin)
+admin.site.register(CheckIn, CheckInAdmin)
+admin.site.register(Email, EmailAdmin)
+admin.site.register(EmailTraveller, EmailTravellerAdmin)
+
